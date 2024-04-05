91fd177215616b24deeef0878b107ec319c77ec5
Elikem Medehou <elikem@Latitude-7420-81920f50.localdomain>
2024-04-05 16:50:09 +0100

    first commit

---
+++ components/Page404.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react'
+
+import * as types from '@/lib/types'
+
+import { PageHead } from './PageHead'
+import styles from './styles.module.css'
+
+export const Page404: React.FC<types.PageProps> = ({ site, pageId, error }) => {
+  const title = site?.name || 'Notion Page Not Found'
+
+  return (
+    <>
+      <PageHead site={site} title={title} />
+
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <h1>Cette page est introuvable</h1>
+
+          <p>La page que vous demandez est introuvable</p>
+
+          <img
+            src='/404.png'
+            alt='404 Not Found'
+            className={styles.errorImage}
+          />
+        </main>
+      </div>
+    </>
+  )
+}
---
+++ components/PageSocial.tsx
@@ -0,0 +1,111 @@
+import * as React from 'react'
+
+import cs from 'classnames'
+
+import * as config from '@/lib/config'
+
+import styles from './PageSocial.module.css'
+
+interface SocialLink {
+  name: string
+  title: string
+  icon: React.ReactNode
+  href?: string
+}
+
+const socialLinks: SocialLink[] = [
+  config.twitter && {
+    name: 'twitter',
+    href: `https://twitter.com/${config.twitter}`,
+    title: `Twitter @${config.twitter}`,
+    icon: (
+      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
+        <path d='M23.44 4.83c-.8.37-1.5.38-2.22.02.93-.56.98-.96 1.32-2.02-.88.52-1.86.9-2.9 1.1-.82-.88-2-1.43-3.3-1.43-2.5 0-4.55 2.04-4.55 4.54 0 .36.03.7.1 1.04-3.77-.2-7.12-2-9.36-4.75-.4.67-.6 1.45-.6 2.3 0 1.56.8 2.95 2 3.77-.74-.03-1.44-.23-2.05-.57v.06c0 2.2 1.56 4.03 3.64 4.44-.67.2-1.37.2-2.06.08.58 1.8 2.26 3.12 4.25 3.16C5.78 18.1 3.37 18.74 1 18.46c2 1.3 4.4 2.04 6.97 2.04 8.35 0 12.92-6.92 12.92-12.93 0-.2 0-.4-.02-.6.9-.63 1.96-1.22 2.56-2.14z' />
+      </svg>
+    )
+  },
+
+  config.github && {
+    name: 'github',
+    href: `https://github.com/${config.github}`,
+    title: `GitHub @${config.github}`,
+    icon: (
+      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
+        <path d='M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22'></path>
+      </svg>
+    )
+  },
+
+  config.linkedin && {
+    name: 'linkedin',
+    href: `https://www.linkedin.com/in/${config.linkedin}`,
+    title: `LinkedIn ${config.author}`,
+    icon: (
+      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
+        <path d='M6.5 21.5h-5v-13h5v13zM4 6.5C2.5 6.5 1.5 5.3 1.5 4s1-2.4 2.5-2.4c1.6 0 2.5 1 2.6 2.5 0 1.4-1 2.5-2.6 2.5zm11.5 6c-1 0-2 1-2 2v7h-5v-13h5V10s1.6-1.5 4-1.5c3 0 5 2.2 5 6.3v6.7h-5v-7c0-1-1-2-2-2z' />
+      </svg>
+    )
+  },
+
+  config.medium && {
+    name: 'medium',
+    href: `https://www.medium.com/@${config.medium}`,
+    title: `Medium ${config.author}`,
+    icon: (
+      <svg
+        width='24'
+        height='24'
+        xmlns='http://www.w3.org/2000/svg'
+        fillRule='evenodd'
+        clipRule='evenodd'
+      >
+        <path d='M2.846 6.887c.03-.295-.083-.586-.303-.784l-2.24-2.7v-.403h6.958l5.378 11.795 4.728-11.795h6.633v.403l-1.916 1.837c-.165.126-.247.333-.213.538v13.498c-.034.204.048.411.213.537l1.871 1.837v.403h-9.412v-.403l1.939-1.882c.19-.19.19-.246.19-.537v-10.91l-5.389 13.688h-.728l-6.275-13.688v9.174c-.052.385.076.774.347 1.052l2.521 3.058v.404h-7.148v-.404l2.521-3.058c.27-.279.39-.67.325-1.052v-10.608z' />
+      </svg>
+    )
+  },
+
+  config.newsletter && {
+    name: 'newsletter',
+    href: `${config.newsletter}`,
+    title: `Newsletter ${config.author}`,
+    icon: (
+      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
+        <path d='M12 .64L8.23 3H5V5L2.97 6.29C2.39 6.64 2 7.27 2 8V18C2 19.11 2.9 20 4 20H20C21.11 20 22 19.11 22 18V8C22 7.27 21.61 6.64 21.03 6.29L19 5V3H15.77M7 5H17V9.88L12 13L7 9.88M8 6V7.5H16V6M5 7.38V8.63L4 8M19 7.38L20 8L19 8.63M8 8.5V10H16V8.5Z' />
+      </svg>
+    )
+  },
+
+  config.youtube && {
+    name: 'youtube',
+    href: `https://www.youtube.com/${config.youtube}`,
+    title: `YouTube ${config.youtube}`,
+    icon: (
+      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
+        <path d='M10,15L15.19,12L10,9V15M21.56,7.17C21.69,7.64 21.78,8.27 21.84,9.07C21.91,9.87 21.94,10.56 21.94,11.16L22,12C22,14.19 21.84,15.8 21.56,16.83C21.31,17.73 20.73,18.31 19.83,18.56C19.36,18.69 18.5,18.78 17.18,18.84C15.88,18.91 14.69,18.94 13.59,18.94L12,19C7.81,19 5.2,18.84 4.17,18.56C3.27,18.31 2.69,17.73 2.44,16.83C2.31,16.36 2.22,15.73 2.16,14.93C2.09,14.13 2.06,13.44 2.06,12.84L2,12C2,9.81 2.16,8.2 2.44,7.17C2.69,6.27 3.27,5.69 4.17,5.44C4.64,5.31 5.5,5.22 6.82,5.16C8.12,5.09 9.31,5.06 10.41,5.06L12,5C16.19,5 18.8,5.16 19.83,5.44C20.73,5.69 21.31,6.27 21.56,7.17Z' />
+      </svg>
+    )
+  }
+].filter(Boolean)
+
+export const PageSocial: React.FC = () => {
+  return (
+    <div className={styles.pageSocial}>
+      {socialLinks.map((action) => (
+        <a
+          className={cs(styles.action, styles[action.name])}
+          href={action.href}
+          key={action.name}
+          title={action.title}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <div className={styles.actionBg}>
+            <div className={styles.actionBgPane} />
+          </div>
+
+          <div className={styles.actionBg}>{action.icon}</div>
+        </a>
+      ))}
+    </div>
+  )
+}
